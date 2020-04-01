0c8e831c2d7ad94c38ea1bb8a9f7f657b0bf9e61
shun-shobon <shun.shobon@gmail.com>
2020-04-01 23:06:53 +0900

    feat: add line button

--- hosting/src/views/Home.vue
+++ hosting/src/views/Home.vue
@@ -1,3 +1,43 @@
 <template>
-  <div id="home" />
+  <div id="home">
+    <b class="description">
+      AtCoderのコンテストについてお知らせします。現在はLINEのみ対応です。
+    </b>
+    <button class="signup-line">LINEで登録</button>
+  </div>
 </template>
+
+<style lang="scss">
+#home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+  * {
+    margin: 1rem 1rem;
+  }
+  .signup-line {
+    border: 0;
+    border-radius: 5px;
+    background: #02b202;
+    color: #fff;
+    padding: 0.5rem 2rem;
+    font-size: 1.5rem;
+    min-width: 10rem;
+    cursor: pointer;
+    &:hover {
+      background: #00a300;
+    }
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 4px #c2e7bd;
+    }
+    &[disabled] {
+      color: #dddddd;
+      background: #808080;
+      cursor: not-allowed;
+    }
+  }
+}
+</style>

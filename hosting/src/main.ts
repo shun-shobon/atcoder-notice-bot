1503dcd834be1b637d8568b7241bc84516016a9e
shun-shobon <shun.shobon@gmail.com>
2020-04-03 00:46:27 +0900

    build: add axios on vue

--- hosting/src/main.ts
+++ hosting/src/main.ts
@@ -2,10 +2,14 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+import axios from "axios"
+import VueAxios from "vue-axios"
 
 import "reset-css"
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
